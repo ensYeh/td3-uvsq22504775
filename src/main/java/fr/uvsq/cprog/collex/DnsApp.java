56a0fbd74070b44c5935e7856c3ab6794a423505
Manal bouzeggaoui <bouzeggaouimanal@gmail.com>
2025-10-18 22:13:38 +0200

    ajouter la classe DnsApp

--- src/main/java/fr/uvsq/cprog/collex/DnsApp.java
+++ src/main/java/fr/uvsq/cprog/collex/DnsApp.java
@@ -1,1 +1,29 @@
 
+import java.io.IOException;
+import java.nio.file.Paths;
+
+import fr.uvsq.cprog.collex.Commande;
+import fr.uvsq.cprog.collex.CommandeQuitter;
+import fr.uvsq.cprog.collex.Dns;
+import fr.uvsq.cprog.collex.DnsTUI;
+
+public class DnsApp {
+    public static void main(String[] args) {
+        try {
+            Dns dns = new Dns(Paths.get("dns.txt"));  
+            DnsTUI tui = new DnsTUI(dns);            
+            boolean continuer = true;
+            while (continuer) {
+                Commande cmd = tui.nextCommande();
+                if (cmd != null) {
+                    cmd.execute();
+                    if (cmd instanceof CommandeQuitter) {
+                        continuer = false;
+                    }
+                }
+            }
+        } catch (IOException e) {
+             System.err.println("Erreur : " + e.getMessage());
+        }
+    }
+}
